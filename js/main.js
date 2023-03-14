4df3bd545d62d43e707e1632f83ca1a94b75d5f2
Giotreglia <giovanni.treglia@gmail.com>
2023-03-14 14:30:52 +0100

    Esercizio + bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Vue è una figata pazzesca!',
+        image_src: 'https://primabergamo.it/media/2017/07/fantozzi-02.gif',
+        image_alt: 'Fantozzi'
       }
     }
   }).mount('#app')
